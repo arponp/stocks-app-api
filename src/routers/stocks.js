9ecba13da380a85aa89e62f4fac611c9d1e90260
Arpon Purkayastha <arponp30@gmail.com>
2021-02-07 10:58:28 -0800

    add endpoint for price and ticker info

--- src/routers/stocks.js
+++ src/routers/stocks.js
@@ -1,12 +1,31 @@
 import express from "express";
-import got from "got";
+import axios from "axios";
 const router = new express.Router();
 
-
-router.get('/stocks/:ticker', async (req, res) => {
+// info about ticker e.g company, exchange, 
+router.get('/stocks/tickers/info', async (req, res) => {
     try {
-        const response = await got(`http://api.marketstack.com/v1/eod?access_key=22f7bd893eabb35ec7875a31ca051a7f&symbols=${req.params.ticker}`);
-        res.send(response);
+        const response = await axios.get(`http://api.marketstack.com/v1/tickers?access_key=22f7bd893eabb35ec7875a31ca051a7f&symbols=${req.query.symbol}`);
+        res.send(response.data);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+//price
+router.get('/stocks/tickers/price', async (req, res) => {
+    try {
+        const date = new Date();
+        const day = date.getDay();
+        let response;
+        if (day == 0 || day == 7) {
+            // eod
+            response = await axios.get(`http://api.marketstack.com/v1/eod?access_key=22f7bd893eabb35ec7875a31ca051a7f&symbols=${req.query.symbol}`);
+        } else {
+            //intraday
+            response = await axios.get(`http://api.marketstack.com/v1/intraday?access_key=22f7bd893eabb35ec7875a31ca051a7f&symbols=${req.query.symbol}`);
+        }
+        res.status(200).send(response.data);
     } catch (e) {
         res.status(400).send(e);
     }
