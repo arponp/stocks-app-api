74df3b9441f56fe8e2ed3ecc0fb64c8a5b299d8c
Arpon Purkayastha <arponp30@gmail.com>
2021-03-25 11:03:05 -0700

    more modifications to user schema

--- src/models/user.js
+++ src/models/user.js
@@ -39,6 +39,52 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
+userSchema.methods.toJSON = () => {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+userSchema.methods.generateAuthToken = async () => {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+
+  return user;
+};
+
+userSchema.pre("save", async (next) => {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
 const User = mongoose.model("User", userSchema, "users");
 
 export default User;
